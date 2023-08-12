7c225230f8d2c68852f16e4ec4d03d2935ec78a6
cnx-afraniogarcia <afranio.garcia@conexasaude.com.br>
2023-08-12 19:34:33 -0300

    base para o projeto backend

---
+++ backend/getme/src/test/java/com/aag/getme/GetmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aag.getme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
