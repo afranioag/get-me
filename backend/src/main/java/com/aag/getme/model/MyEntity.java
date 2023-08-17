9ea1d19039e67173a5f65cde604b3a93876dbd0d
cnx-afraniogarcia <afranio.garcia@conexasaude.com.br>
2023-08-17 02:09:46 -0300

    Equals e hashcode para myEntity

--- backend/src/main/java/com/aag/getme/model/MyEntity.java
+++ backend/src/main/java/com/aag/getme/model/MyEntity.java
@@ -7,18 +7,35 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class MyEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MyEntity other = (MyEntity) obj;
+        return Objects.equals(id, other.id);
+    }
 }
 
