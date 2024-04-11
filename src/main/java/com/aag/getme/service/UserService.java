cb67bce68bdd1355add770a47f4085f4207cb807
afranioag <afranio.garcia@ccc.ufcg.edu.br>
2024-04-11 01:57:35 -0300

    modificando forma de injetar bcrypt

--- src/main/java/com/aag/getme/service/UserService.java
+++ src/main/java/com/aag/getme/service/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +29,8 @@ public class UserService implements UserDetailsService {
     private ModelMapper modelMapper;
 
     @Autowired
-    private BCryptPasswordEncoder encoder;
+    private PasswordEncoder encoder;
+
     @Autowired
     private UserRepository userRepository;
 
